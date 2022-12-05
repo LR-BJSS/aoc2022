f8545217f71149d88bec234d828612e80749fb5b
leigh.rankin <leigh.rankin@bjss.com>
2022-12-05 15:45:26 +0000

    Day 5 refactor and correct example input whitespace

--- day5.rb
+++ day5.rb
@@ -2,12 +2,12 @@ class Main
   attr_reader :input
   MoveCommand = Struct.new(:amount, :from, :to)
   def initialize(input)
-    raw_crate_stacks, raw_move_commands = input.split("\n\n")
-    @crate_stacks = parse_raw_crate_stacks(raw_crate_stacks)
-    @move_commands = parse_raw_move_commands(raw_move_commands)
+    raw_stacks, raw_commands = input.split("\n\n")
+    @stacks = parse_raw_stacks(raw_stacks)
+    @commands = parse_raw_commands(raw_commands)
   end
-  def parse_raw_crate_stacks(raw_crate_stacks)
-    raw_crate_stacks
+  def parse_raw_stacks(raw_stacks)
+    raw_stacks
       .split("\n")
       .map(&:chars)
       .transpose
@@ -16,8 +16,8 @@ class Main
       .map { _1[0..-2].chars.reverse }
   end
 
-  def parse_raw_move_commands(raw_move_commands)
-    raw_move_commands
+  def parse_raw_commands(raw_commands)
+    raw_commands
       .split("\n")
       .map { |line|
         line
@@ -31,28 +31,25 @@ class Main
 
   def run_crane(crate_stacks, move_commands, strategy)
     stack = crate_stacks.map(&:clone)
-
     move_commands.each do |move|
       stack[move.from]
         .pop(move.amount)
         .send(strategy)
         .then { stack[move.to].concat _1 }
     end
-
     stack.map(&:last).join
   end
 
   def calculatePt1
-    run_crane(@crate_stacks, @move_commands, :reverse)
+    run_crane(@stacks, @commands, :reverse)
     end
 
   def calculatePt2
-    run_crane(@crate_stacks, @move_commands, :itself)
+    run_crane(@stacks, @commands, :itself)
     end
 end
 
-#puts "Day5 Pt1 Example: #{Main.new(File.open('day5-example.txt').read).calculatePt1}"
-#puts "Day5 Pt2 Example: #{Main.new(File.open('day5-example.txt').read).calculatePt2}"
+puts "Day5 Pt1 Example: #{Main.new(File.open('day5-example.txt').read).calculatePt1}"
+puts "Day5 Pt2 Example: #{Main.new(File.open('day5-example.txt').read).calculatePt2}"
 puts "Day5 Pt1 Input: #{Main.new(File.open('day5-input.txt').read).calculatePt1}"
 puts "Day5 Pt2 Input: #{Main.new(File.open('day5-input.txt').read).calculatePt2}"
-
