e7740aaa922013dbddb49c4ee722dc52b5929ef3
leigh.rankin <leigh.rankin@bjss.com>
2022-12-06 10:37:28 +0000

    Day 6 refactor

--- day6.rb
+++ day6.rb
@@ -5,26 +5,17 @@ class Main
     @input = input
   end
 
-  def calculatePt1
-    i = 3
-    @input.chars.each_cons(4) do |q|
-      i += 1
-      break if q.uniq.count == q.count
+  def calculate(marker)
+    start_of_message = marker - 1               # we can start at 'marker' - 1 as it isn't possible any earlier
+    @input.chars.each_cons(marker) do |message| # iterate for each consecutive message that is 'marker' long
+      start_of_message += 1
+      break if message.uniq.count == message.count # checks that everything in the message is unique
     end
-    return i
-  end
-
-  def calculatePt2
-    j = 13
-    @input.chars.each_cons(14) do |q|
-      j += 1
-      break if q.uniq.count == q.count
-    end
-    return j
+    return start_of_message
   end
 end
 
-puts "Day6 Pt1 Example: #{Main.new(File.open('day6-example.txt').read).calculatePt1}"
-puts "Day6 Pt2 Example: #{Main.new(File.open('day6-example.txt').read).calculatePt2}"
-puts "Day6 Pt1 Input: #{Main.new(File.open('day6-input.txt').read).calculatePt1}"
-puts "Day6 Pt2 Input: #{Main.new(File.open('day6-input.txt').read).calculatePt2}"
+puts "Day6 Pt1 Example: #{Main.new(File.open('day6-example.txt').read).calculate(4)}"
+puts "Day6 Pt2 Example: #{Main.new(File.open('day6-example.txt').read).calculate(14)}"
+puts "Day6 Pt1 Input: #{Main.new(File.open('day6-input.txt').read).calculate(4)}"
+puts "Day6 Pt2 Input: #{Main.new(File.open('day6-input.txt').read).calculate(14)}"
