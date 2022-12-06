37eba9a820b56b5afffb630bc6e75848ec98409b
leigh.rankin <leigh.rankin@bjss.com>
2022-12-06 17:15:59 +0000

    Day 5 tidy up

--- day5.rb
+++ day5.rb
@@ -1,11 +1,13 @@
 class Main
   attr_reader :input
   MoveCommand = Struct.new(:amount, :from, :to)
+  
   def initialize(input)
     raw_stacks, raw_commands = input.split("\n\n")
     @stacks = parse_raw_stacks(raw_stacks)
     @commands = parse_raw_commands(raw_commands)
   end
+  
   def parse_raw_stacks(raw_stacks)
     raw_stacks
       .split("\n")
@@ -40,16 +42,16 @@ class Main
     stack.map(&:last).join
   end
 
-  def calculatePt1
+  def calculatePart1
     run_crane(@stacks, @commands, :reverse)
     end
 
-  def calculatePt2
+  def calculatePart2
     run_crane(@stacks, @commands, :itself)
     end
 end
 
-puts "Day5 Pt1 Example: #{Main.new(File.open('day5-example.txt').read).calculatePt1}"
-puts "Day5 Pt2 Example: #{Main.new(File.open('day5-example.txt').read).calculatePt2}"
-puts "Day5 Pt1 Input: #{Main.new(File.open('day5-input.txt').read).calculatePt1}"
-puts "Day5 Pt2 Input: #{Main.new(File.open('day5-input.txt').read).calculatePt2}"
+puts "Day5 Pt1 Example: #{Main.new(File.open('day5-example.txt').read).calculatePart1}"
+puts "Day5 Pt2 Example: #{Main.new(File.open('day5-example.txt').read).calculatePart2}"
+puts "Day5 Pt1 Input: #{Main.new(File.open('day5-input.txt').read).calculatePart1}"
+puts "Day5 Pt2 Input: #{Main.new(File.open('day5-input.txt').read).calculatePart2}"
