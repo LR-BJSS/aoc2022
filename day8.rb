24cb50dee40d37ec90c7597a99289768985eb80f
leigh.rankin <f4780y.limited@gmail.com>
2022-12-08 07:20:37 +0000

    Day 8 initial solution

---
+++ day8.rb
@@ -0,0 +1,50 @@
+class Main
+  attr_reader :input
+
+  def initialize(input)
+    @grid = []
+    input.readlines.each do |line|
+      break if line == "\n"
+      @grid << line.chomp.split('').map(&:to_i)
+    end
+    @grid_t = @grid.transpose
+  end
+
+  def calculate_part1
+    ans = @grid.length * 4 - 4
+    (1..@grid.length - 2).each do |i|
+      (1..@grid.length - 2).each do |j|
+        left = @grid[i][0..j - 1].all? { |t| t < @grid[i][j] }
+        right = @grid[i][j + 1..-1].all? { |t| t < @grid[i][j] }
+        up = @grid_t[j][0..i - 1].all? { |t| t < @grid[i][j] }
+        down = @grid_t[j][i + 1..-1].all? { |t| t < @grid[i][j] }
+        ans += 1 if left || right || up || down
+      end
+    end
+    ans
+  end
+
+  def calculate_part2
+    ans = 1
+    (1..@grid.length - 2).each do |i|
+      (1..@grid.length - 2).each do |j|
+        left = @grid[i][0..j - 1].reverse.find_index { |t| t >= @grid[i][j] }
+        left = left ? left + 1 : @grid[i][0..j - 1].length
+        right = @grid[i][j + 1..-1].find_index { |t| t >= @grid[i][j] }
+        right = right ? right + 1 : @grid[i][j + 1..-1].length
+        up = @grid_t[j][0..i - 1].reverse.find_index { |t| t >= @grid[i][j] }
+        up = up ? up + 1 : @grid_t[j][0..i - 1].length
+        down = @grid_t[j][i + 1..-1].find_index { |t| t >= @grid[i][j] }
+        down = down ? down + 1 : @grid_t[j][i + 1..-1].length
+        tmp = left * right * up * down
+        ans = tmp if tmp > ans
+      end
+    end
+    ans
+  end
+end
+
+puts "Day8 Part1 Example: #{Main.new(File.open('day8-example.txt')).calculate_part1}"
+puts "Day8 Part2 Example: #{Main.new(File.open('day8-example.txt')).calculate_part2}"
+puts "Day8 Part1 Input: #{Main.new(File.open('day8-input.txt')).calculate_part1}"
+puts "Day8 Part2 Input: #{Main.new(File.open('day8-input.txt')).calculate_part2}"
