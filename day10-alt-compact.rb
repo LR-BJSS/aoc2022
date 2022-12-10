0d619f662a9a61f57bdafbbdf3fbee6b75f1e1a5
leigh.rankin <f4780y.limited@gmail.com>
2022-12-10 17:55:06 +0000

    Day 10 alternative compact solution

---
+++ day10-alt-compact.rb
@@ -0,0 +1,9 @@
+h = File.readlines('./input/day10-input.txt', chomp: true).each_with_object([1]) do |i, a|
+  a.push a.last
+  a.push(a.last + i.split.last.to_i) if i.start_with? 'addx'
+end
+puts 20.step(by: 40, to: 220).sum { |i| i * h[i - 1] }
+240.times
+   .map { |i| [i % 40, (h[i] - 1).upto(h[i] + 1)] }
+   .map { |i, j| j.include?(i) ? '█' : '░' }
+   .each_slice(40) { |i| puts i.join }
