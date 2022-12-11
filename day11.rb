c90236db30d0ac0509b512cc7d1623833e2e3bda
leigh.rankin <f4780y.limited@gmail.com>
2022-12-11 19:53:15 +0000

    Day 11 further refactoring

--- day11.rb
+++ day11.rb
@@ -9,26 +9,28 @@ class Main
   end
 
   def parse(input)
-    @monkeys = input.strip.split("\n\n").map do |monkey_business|
-      lines = monkey_business.split("\n")
+    @monkeys = input.strip.split("\n\n").map do |monkey_map|
+      business = monkey_map.split("\n")
       {
-        items: lines[1].scan(/\d+/).map(&:to_i),
-        operation: lines[2].then do |op|
-          next [:pow, 2] if op.scan(/old \W old/).first
-          [lines[2].scan(/[+*]/).first.to_sym, lines[2].scan(/\d+$/).map(&:to_i).first]
+        items: business[1].scan(/\d+/).map(&:to_i),
+        operation: business[2].then do |op|
+          next [:pow, 2] if op.scan(/old [*] old/).first
+          [business[2].scan(/[+*]/).first.to_sym, business[2].scan(/\d+$/).map(&:to_i).first]
         end,
-        test: lines[3].scan(/\d+$/).first.to_i,
-        if_true: lines[4].scan(/\d+$/).first.to_i,
-        if_false: lines[5].scan(/\d+$/).first.to_i,
+        test: business[3].scan(/\d+$/).first.to_i,
+        if_true: business[4].scan(/\d+$/).first.to_i,
+        if_false: business[5].scan(/\d+$/).first.to_i,
         inspections: 0
       }
     end
 
-    def monkey_business(relief_factor, divisor)
+    def monkey_business(relief_factor)
+      # common_divisor = @monkeys.map { |m| m[:test] }.inject(&:*) # simple product of divisors (since all primes)
+      common_divisor = @monkeys.map { |m| m[:test] }.reduce(1, :lcm) # using lowest common mulitplier function
       @monkeys.each do |monkey|
         monkey[:inspections] += monkey[:items].length
         monkey[:items].each do |initial_level|
-          worry_level = initial_level.send(*monkey[:operation]) / relief_factor % divisor
+          worry_level = initial_level.send(*monkey[:operation]) / relief_factor % common_divisor
           throw_to = (worry_level % monkey[:test]) == 0 ? monkey[:if_true] : monkey[:if_false]
           @monkeys[throw_to][:items].push(worry_level)
         end
@@ -36,18 +38,16 @@ class Main
       end
     end
 
-    def calculate_part1
-      common_divisor = @monkeys.map { |m| m[:test] }.inject(&:*) # simple product of divisors (all primes)
+    def calculate_part1(relief_factor)
       20.times do
-        monkey_business(3, common_divisor)
+        monkey_business(relief_factor)
       end
       @monkeys.map { |m| m[:inspections] }.sort.reverse.take(2).inject(&:*)
     end
 
-    def calculate_part2
-      common_divisor = @monkeys.map { |m| m[:test] }.reduce(1, :lcm) # lowest common mulitplier function
+    def calculate_part2(relief_factor)
       10000.times do
-        monkey_business(1, common_divisor)
+        monkey_business(relief_factor)
       end
       @monkeys.map { |m| m[:inspections] }.sort.reverse.take(2).inject(&:*)
     end
@@ -56,28 +56,28 @@ class Main
   class Test_Day_11 < Minitest::Test
     def test_calculate_part1_example
       test_case = 10605
-      assert_equal(test_case, Main.new(File.open('./input/day11-example.txt').read).calculate_part1)
+      assert_equal(test_case, Main.new(File.open('./input/day11-example.txt').read).calculate_part1(3))
     end
 
     def test_calculate_part2_example
       test_case = 2713310158
-      assert_equal(test_case, Main.new(File.open('./input/day11-example.txt').read).calculate_part2)
+      assert_equal(test_case, Main.new(File.open('./input/day11-example.txt').read).calculate_part2(1))
     end
 
     # input cases added once correct answers are verified by submission to aoc
     def test_calculate_part_input
       test_case = 99840
-      assert_equal(test_case, Main.new(File.open('./input/day11-input.txt').read).calculate_part1)
+      assert_equal(test_case, Main.new(File.open('./input/day11-input.txt').read).calculate_part1(3))
     end
 
     def test_calculate_part2_input
       test_case = 20683044837
-      assert_equal(test_case, Main.new(File.open('./input/day11-input.txt').read).calculate_part2)
+      assert_equal(test_case, Main.new(File.open('./input/day11-input.txt').read).calculate_part2(1))
     end
   end
 end
 
-puts "Day11 Part1 Example: #{Main.new(File.open('./input/day11-example.txt').read).calculate_part1}"
-puts "Day11 Part2 Example: #{Main.new(File.open('./input/day11-example.txt').read).calculate_part2}"
-puts "Day11 Part1 Input: #{Main.new(File.open('./input/day11-input.txt').read).calculate_part1}"
-puts "Day11 Part2 Input: #{Main.new(File.open('./input/day11-input.txt').read).calculate_part2}"
+puts "Day11 Part1 Example: #{Main.new(File.open('./input/day11-example.txt').read).calculate_part1(3)}"
+puts "Day11 Part2 Example: #{Main.new(File.open('./input/day11-example.txt').read).calculate_part2(1)}"
+puts "Day11 Part1 Input: #{Main.new(File.open('./input/day11-input.txt').read).calculate_part1(3)}"
+puts "Day11 Part2 Input: #{Main.new(File.open('./input/day11-input.txt').read).calculate_part2(1)}"
